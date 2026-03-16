78770333d881c549f4c3f53735b832c767f6841d
digiihelpdesk-prog <your@email.com>
2026-03-16 12:54:39 +0530

    Fix api/apply route and CORS

--- my-backend/index.js
+++ my-backend/index.js
@@ -14,4 +14,10 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/candidates', require('./routes/candidateRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Nursing Backend is running! 🚀');
+});
